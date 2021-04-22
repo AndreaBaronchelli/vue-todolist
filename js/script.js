1c7f58a5224951e638538baa380cf1012143d6e2
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-22 14:17:21 +0200

    some style, first script

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        todos: [
+            {
+                text: "Change Light Saber",
+                completed: false,
+            },
+            {
+                text: "Murder Padawans",
+                completed: false,
+            },
+            {
+                text: "Call Mom (Nevermind, She's Dead)",
+                completed: true,
+            },
+            {
+                text: "Palpatine's B-Day",
+                completed: false,
+            },
+            {
+                text: "Change Tie Fighter Oil",
+                completed: false,
+            },
+            {
+                text: "Polish Helmet",
+                completed: false,
+            },
+            {
+                text: "Destroy Alderaan",
+                completed: false,
+            },
+        ]
+    },
+    methods: {},
+});
